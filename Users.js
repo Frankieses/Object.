bff91351d6cbde2d93a49c54c5043d6a631c7a61
Dave Francis C. Baugbog <124759430+Frankieses@users.noreply.github.com>
2023-11-27 20:11:22 +0800

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,203 @@
+export const Users = [
+  {
+    id: 1,
+    name: {
+      firstname: "John",
+      lastname: "Doe",
+    },
+    course: "CS",
+  },
+  {
+    id: 2,
+    name: {
+      firstname: "Alice",
+      lastname: "Smith",
+    },
+    course: "IT",
+  },
+  {
+    id: 3,
+    name: {
+      firstname: "Bob",
+      lastname: "Johnson",
+    },
+    course: "SE",
+  },
+  {
+    id: 4,
+    name: {
+      firstname: "Eva",
+      lastname: "Williams",
+    },
+    course: "CE",
+  },
+  {
+    id: 5,
+    name: {
+      firstname: "Michael",
+      lastname: "Jones",
+    },
+    course: "DS",
+  },
+  {
+    id: 6,
+    name: {
+      firstname: "Sophia",
+      lastname: "Brown",
+    },
+    course: "CS",
+  },
+  {
+    id: 7,
+    name: {
+      firstname: "Matthew",
+      lastname: "Davis",
+    },
+    course: "IT",
+  },
+  {
+    id: 8,
+    name: {
+      firstname: "Olivia",
+      lastname: "Miller",
+    },
+    course: "SE",
+  },
+  {
+    id: 9,
+    name: {
+      firstname: "Daniel",
+      lastname: "Taylor",
+    },
+    course: "CE",
+  },
+  {
+    id: 10,
+    name: {
+      firstname: "Jessica",
+      lastname: "Moore",
+    },
+    course: "DS",
+  },
+  {
+    id: 11,
+    name: {
+      firstname: "Nathan",
+      lastname: "Clark",
+    },
+    course: "CS",
+  },
+  {
+    id: 12,
+    name: {
+      firstname: "Emily",
+      lastname: "Wilson",
+    },
+    course: "IT",
+  },
+  {
+    id: 13,
+    name: {
+      firstname: "Christopher",
+      lastname: "Martin",
+    },
+    course: "SE",
+  },
+  {
+    id: 14,
+    name: {
+      firstname: "Ava",
+      lastname: "Anderson",
+    },
+    course: "CE",
+  },
+  {
+    id: 15,
+    name: {
+      firstname: "Brandon",
+      lastname: "White",
+    },
+    course: "DS",
+  },
+  {
+    id: 16,
+    name: {
+      firstname: "Hannah",
+      lastname: "Thompson",
+    },
+    course: "CS",
+  },
+  {
+    id: 17,
+    name: {
+      firstname: "Samuel",
+      lastname: "Harris",
+    },
+    course: "IT",
+  },
+  {
+    id: 18,
+    name: {
+      firstname: "Madison",
+      lastname: "Walker",
+    },
+    course: "SE",
+  },
+  {
+    id: 19,
+    name: {
+      firstname: "Logan",
+      lastname: "Turner",
+    },
+    course: "CE",
+  },
+  {
+    id: 20,
+    name: {
+      firstname: "Grace",
+      lastname: "Scott",
+    },
+    course: "DS",
+  },
+  {
+    id: 21,
+    name: {
+      firstname: "Ryan",
+      lastname: "Mitchell",
+    },
+    course: "CS",
+  },
+  {
+    id: 22,
+    name: {
+      firstname: "Lily",
+      lastname: "Williams",
+    },
+    course: "IT",
+  },
+  {
+    id: 23,
+    name: {
+      firstname: "Caleb",
+      lastname: "Johnson",
+    },
+    course: "SE",
+  },
+  {
+    id: 24,
+    name: {
+      firstname: "Emma",
+      lastname: "Brown",
+    },
+    course: "CE",
+  },
+  {
+    id: 25,
+    name: {
+      firstname: "Owen",
+      lastname: "Jones",
+    },
+    course: "DS",
+  },
+  
+];
